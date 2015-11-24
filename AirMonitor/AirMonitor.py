5bc03721f29494cadf0708c2a67675dc31f45469
BuiTran <tran.n.bui@gmail.com>
2015-11-24 17:28:35 -0600

    Create AirMonitor package

---
+++ AirMonitor/AirMonitor.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 24, 2015
+
+@author: tran
+'''
+
+class AirMonitor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
