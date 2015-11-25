5986e121fbeaa81d60af1b512cf0f0e4d34859dd
BuiTran <tran.n.bui@gmail.com>
2015-11-25 15:13:55 -0600

    Air Data Collector

--- AirMonitor/AirDataCollector.py
+++ AirMonitor/AirDataCollector.py
@@ -3,7 +3,8 @@ Created on Nov 24, 2015
 
 @author: tran
 '''
-
+import os.path
+from AirMonitor import AirMonitor
 class AirDataCollector(object):
     '''
     Our AirDataCollector class allows us to access a specified data monitor from the collection of monitors. An
@@ -17,4 +18,30 @@ class AirDataCollector(object):
         Constructor
         '''
         self.dict = {}
+        self.readFile()
+        
+    def readFile(self):
+        f = open("text.txt")
+        lines = f.read().splitlines()
+        for text in lines:
+            listWord = text.split()
+            id = int(listWord[0])
+            name = listWord[1]
+            lo = float(listWord[2])
+            la = float(listWord[3])
+            metric = listWord([4])
+            dataList = [int(s) for s in listWord[5:]]
+            airMonitor = AirMonitor(id, name, lo, la, metric, dataList)
+            self.dict[id] = airMonitor
+    
+    def getMonitorList(self):
+        for x in self.dict:
+            print(x)
+    
+    def getMonitorById(self,id):
+        return self.dict[id]
+    
+       
+            
+        
         
